fd2c5a1d56040609b4c0ad95f0dc58c814528f69
James Devlin <devlin.ja@northeastern.edu>
2023-04-16 19:13:23 -0400

    Changed some queries to run

--- flask-app/src/moderators.py
+++ flask-app/src/moderators.py
@@ -8,45 +8,51 @@ moderators = Blueprint("moderators", __name__)
 @moderators.route("/units", methods=["GET"])
 def get_units():
     """Example"""
+    # Can remove this whenever
     return _run_and_respond("SELECT * FROM Units;")
 
 @moderators.route("/users", methods=["GET"])
 def get_users():
     """Gets all the users accounts"""
-    return _run_and_respond(f"SELECT Email, FirstName, LastName, City, State, Zip, Phone, Banned FROM Accounts;")
+    return _run_and_respond("SELECT Email, FirstName, LastName, City, State, Zip, Phone, Banned FROM Accounts;")
 
 @moderators.route("/users", methods=["GET"])
 def get_rentees():
     """Gets all the Rentee users accounts"""
-    return _run_and_respond(f"SELECT Email, FirstName, LastName, City, State, Zip, Phone, Banned FROM Accounts JOIN Rentees ON Accounts.Email == Rentees.AccountEmail;")
+    return _run_and_respond("SELECT Email, FirstName, LastName, City, State, Zip, Phone, Banned FROM Accounts JOIN Rentees ON Accounts.Email = Rentees.AccountEmail;")
 
 @moderators.route("/users", methods=["GET"])
 def get_renters():
     """Gets all the Renter users accounts"""
-    return _run_and_respond(f"SELECT Email, FirstName, LastName, City, State, Zip, Phone, Banned FROM Accounts JOIN Renters ON Accounts.Email == Renters.AccountEmail;")
+    return _run_and_respond("SELECT Email, FirstName, LastName, City, State, Zip, Phone, Banned FROM Accounts JOIN Renters ON Accounts.Email = Renters.AccountEmail;")
 
-@moderators.route("/users/<string:accountEmail>", methods=["PUT"])
+@moderators.route("/users/<accountEmail>", methods=["PUT"])
 def ban_user(accountEmail):
     """Bans the user's account"""
     return _run_and_respond(
         f"UPDATE Accounts SET Banned = 1 WHERE Email = {accountEmail};"
     )
 
-@moderators.route("/users/<string:accountEmail>", methods=["PUT"])
+@moderators.route("/users/<accountEmail>", methods=["PUT"])
 def unban_user(accountEmail):
     """Unbans the user's account"""
     return _run_and_respond(
         f"UPDATE Accounts SET Banned = 0 WHERE Email = {accountEmail};"
     )
 
-@moderators.route("/users/<string:accountEmail>", methods=["GET"])
+@moderators.route("/users/<accountEmail>", methods=["GET"])
 def get_user(accountEmail):
     """Gets the details of a user's account"""
     return _run_and_respond(
-        f"SELECT Email, FirstName, LastName, City, State, Zip, Phone, Banned, CreditCardNumber, ExpiryDate, BankName, AccountName FROM ((SELECT * FROM Accounts WHERE Email = {accountEmail}) LEFT JOIN Rentees ON Accounts.Email = Rentees.AccountEmail) LEFT JOIN Renters ON Accounts.Email = Renters.AccountEmail;"
+        f"""
+        SELECT Email, FirstName, LastName, City, State, Zip, Phone, Banned, CreditCardNumber, ExpiryDate, BankName, AccountName
+        FROM ((SELECT * FROM Accounts WHERE Email = {accountEmail}) AC
+        LEFT JOIN Rentees ON AC.Email = Rentees.AccountEmail)
+        LEFT JOIN Renters ON AC.Email = Renters.AccountEmail;
+        """
     )
 
-@moderators.route("/users/<string:email>", methods=["POST"])
+@moderators.route("/users/<email>", methods=["POST"])
 def new_test_user(email):
     """Creates a new test user"""
     # NOTE: currently, all optional fields are required
@@ -56,11 +62,11 @@ def new_test_user(email):
     lastName = form["lastName"]
     city = form["city"]
     state = form["state"]
-    zip = form["zip"]
+    zipcode = form["zip"]
     phone = form["phone"]
     return _run_and_respond(
         f"""
         INSERT INTO Accounts (Email, Password, FirstName, LastName, City, State, Zip, Phone, Banned)
-        VALUES ('{email}', '{password}', '{firstName}', '{lastName}', '{city}', '{state}', '{zip}', '{phone}', 0);
+        VALUES ('{email}', '{password}', '{firstName}', '{lastName}', '{city}', '{state}', '{zipcode}', '{phone}', 0);
         """
     )
