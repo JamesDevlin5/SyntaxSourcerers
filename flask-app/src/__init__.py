268d0c8dfb3b5a9b6d83cd7564ea378dbb1366a1
James Devlin <devlin.ja@northeastern.edu>
2023-04-16 23:10:38 -0400

    Added tests

--- flask-app/src/__init__.py
+++ flask-app/src/__init__.py
@@ -16,8 +16,8 @@ def create_app():
     app.config["SECRET_KEY"] = "someCrazyS3cR3T!Key.!"
 
     # these are for the DB object to be able to connect to MySQL.
-    app.config["MYSQL_DATABASE_USER"] = "root"
-    app.config["MYSQL_DATABASE_PASSWORD"] = open("/secrets/db_root_password.txt").readline()
+    app.config["MYSQL_DATABASE_USER"] = "not_root"
+    app.config["MYSQL_DATABASE_PASSWORD"] = open("/secrets/db_password.txt").readline()
     app.config["MYSQL_DATABASE_HOST"] = "db"
     app.config["MYSQL_DATABASE_PORT"] = 3306
     app.config["MYSQL_DATABASE_DB"] = "cargocache"
