e43a770cafb9023b98f25c1d35f6cb83e263a422
Zachary-Maizes <zmaizes@gmail.com>
2023-04-16 22:30:37 -0400

    Tweaked Moderator Routes

--- flask-app/src/moderators.py
+++ flask-app/src/moderators.py
@@ -9,37 +9,37 @@ def get_users():
     """Gets all the users accounts"""
     return _run_and_respond("SELECT Email, FirstName, LastName, City, State, Zip, Phone, Banned FROM Accounts;")
 
-@moderators.route("/users", methods=["GET"])
+@moderators.route("/rentees", methods=["GET"])
 def get_rentees():
     """Gets all the Rentee users accounts"""
     return _run_and_respond("SELECT Email, FirstName, LastName, City, State, Zip, Phone, Banned FROM Accounts JOIN Rentees ON Accounts.Email = Rentees.AccountEmail;")
 
-@moderators.route("/users", methods=["GET"])
+@moderators.route("/renters", methods=["GET"])
 def get_renters():
     """Gets all the Renter users accounts"""
     return _run_and_respond("SELECT Email, FirstName, LastName, City, State, Zip, Phone, Banned FROM Accounts JOIN Renters ON Accounts.Email = Renters.AccountEmail;")
 
-@moderators.route("/users/<accountEmail>", methods=["PUT"])
-def ban_user(accountEmail):
+@moderators.route("/users/ban/<email>", methods=["PUT"])
+def ban_user(email):
     """Bans the user's account"""
     return _run_and_respond(
-        f"UPDATE Accounts SET Banned = 1 WHERE Email = {accountEmail};"
+        f"UPDATE Accounts SET Banned = 1 WHERE Email = {email};"
     )
 
-@moderators.route("/users/<accountEmail>", methods=["PUT"])
-def unban_user(accountEmail):
+@moderators.route("/users/unban/<email>", methods=["PUT"])
+def unban_user(email):
     """Unbans the user's account"""
     return _run_and_respond(
-        f"UPDATE Accounts SET Banned = 0 WHERE Email = {accountEmail};"
+        f"UPDATE Accounts SET Banned = 0 WHERE Email = {email};"
     )
 
-@moderators.route("/users/<accountEmail>", methods=["GET"])
-def get_user(accountEmail):
+@moderators.route("/users/<email>", methods=["GET"])
+def get_user(email):
     """Gets the details of a user's account"""
     return _run_and_respond(
         f"""
         SELECT Email, FirstName, LastName, City, State, Zip, Phone, Banned, CreditCardNumber, ExpiryDate, BankName, AccountName
-        FROM ((SELECT * FROM Accounts WHERE Email = {accountEmail}) AC
+        FROM ((SELECT * FROM Accounts WHERE Email = {email}) AC
         LEFT JOIN Rentees ON AC.Email = Rentees.AccountEmail)
         LEFT JOIN Renters ON AC.Email = Renters.AccountEmail;
         """
