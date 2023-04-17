d50782d5710046e0e5fdb6f99e086a24b794206c
Zachary-Maizes <zmaizes@gmail.com>
2023-04-16 21:27:52 -0400

    Removed Deprecated Route

--- flask-app/src/moderators.py
+++ flask-app/src/moderators.py
@@ -4,13 +4,6 @@ from src import _run_and_respond
 
 moderators = Blueprint("moderators", __name__)
 
-
-@moderators.route("/units", methods=["GET"])
-def get_units():
-    """Example"""
-    # Can remove this whenever
-    return _run_and_respond("SELECT * FROM Units;")
-
 @moderators.route("/users", methods=["GET"])
 def get_users():
     """Gets all the users accounts"""
