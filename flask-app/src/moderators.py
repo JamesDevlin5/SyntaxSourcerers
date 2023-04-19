f18f82927fcd7c75749d94f559aefe2e8a21b946
James Devlin <devlin.ja@northeastern.edu>
2023-04-19 10:19:24 -0400

    Fixed Routes

--- flask-app/src/moderators.py
+++ flask-app/src/moderators.py
@@ -31,12 +31,12 @@ def get_users():
 @moderators.route("/rentees", methods=["GET"])
 def get_rentees():
     """Gets all the Rentee users accounts"""
-    return _run_and_respond("SELECT Email, FirstName, LastName, City, State, Zip, Phone, Banned FROM Accounts JOIN Rentees ON Accounts.Email = Rentees.AccountEmail;")
+    return _run_and_respond("SELECT Email, FirstName, LastName, City, State, Zip, Phone FROM Accounts JOIN Rentees ON Accounts.Email = Rentees.AccountEmail;")
 
 @moderators.route("/renters", methods=["GET"])
 def get_renters():
     """Gets all the Renter users accounts"""
-    return _run_and_respond("SELECT Email, FirstName, LastName, City, State, Zip, Phone, Banned FROM Accounts JOIN Renters ON Accounts.Email = Renters.AccountEmail;")
+    return _run_and_respond("SELECT Email, FirstName, LastName, City, State, Zip, Phone FROM Accounts JOIN Renters ON Accounts.Email = Renters.AccountEmail;")
 
 @moderators.route("/users/ban/<email>", methods=["PUT"])
 def ban_user(email):
@@ -57,52 +57,75 @@ def get_user(email):
     """Gets the details of a user's account"""
     return _run_and_respond(
         f"""
-        SELECT Email, FirstName, LastName, City, State, Zip, Phone, Banned, CreditCardNumber, ExpiryDate, BankName, AccountName
+        SELECT Email, FirstName, LastName, City, State, Zip, Phone, CreditCardNumber, ExpiryDate, BankName, AccountName
         FROM ((SELECT * FROM Accounts WHERE Email = '{email}') AC
         LEFT JOIN Rentees ON AC.Email = Rentees.AccountEmail)
         LEFT JOIN Renters ON AC.Email = Renters.AccountEmail;
         """
     )
+
 @moderators.route("/users/<email>", methods=["DELETE"])
 def delete_account(email):
     """Permanently deletes an account"""
     return _run_and_respond(
-        f"""DELETE FROM Accounts WHERE Email = {email};
-        """
+        f"DELETE FROM Accounts WHERE Email = '{email}';"
     )
-@moderators.route("/units/<unitID>", methods=["DELETE"])
+
+@moderators.route("/units/<int:unitID>", methods=["DELETE"])
 def delete_listing(unitID):
     """Permanently deletes an unit listing"""
     return _run_and_respond(
-        f"""DELETE FROM Units WHERE UnitID = {unitID};
-        """
+        f"DELETE FROM Units WHERE UnitID = {unitID};"
     )
 @moderators.route("/users/<accountEmail>", methods=["PUT"])
 def new_test_user(email):
     """Updates a user's account information"""
     # NOTE: currently, all optional fields are required
     form = request.form
-    password = form.get("password", None)
-    firstName = form.get("firstName", None)
-    lastName = form.get("lastName", None)
-    city = form.get("city", None)
-    state = form.get("state", None)
-    zipcode = form.get("zip", None)
-    phone = form.get("phone", None)
+    if "password" in form:
+        password = "'" + form["password"] + "'"
+    else:
+        password = "NULL"
+    if "firstName" in form:
+        firstName = "'" + form["firstName"] + "'"
+    else:
+        firstName = "NULL"
+    if "lastName" in form:
+        lastName = "'" + form["lastName"] + "'"
+    else:
+        lastName = "NULL"
+    if "city" in form:
+        city = "'" + form["city"] + "'"
+    else:
+        city = "NULL"
+    if "state" in form:
+        state = "'" + form["state"] + "'"
+    else:
+        state = "NULL"
+    if "zip" in form:
+        zipcode = "'" + form["zip"] + "'"
+    else:
+        zipcode = "NULL"
+    if "phone" in form:
+        phone = "'" + form["phone"] + "'"
+    else:
+        phone = "NULL"
+
     return _run_and_respond(
         f"""
         UPDATE Accounts 
-SET 
-  Password = CASE WHEN {password} IS NOT NULL THEN {password} ELSE Password END,
-  FirstName = CASE WHEN {firstName} IS NOT NULL THEN {firstName} ELSE FirstName END,
-  LastName = CASE WHEN {lastName} IS NOT NULL THEN {lastName} ELSE LastName END,
-  City = CASE WHEN {city} IS NOT NULL THEN {city} ELSE City END,
-  State = CASE WHEN {state} IS NOT NULL THEN {state} ELSE State END,
-  Phone = CASE WHEN {phone} IS NOT NULL THEN {phone} ELSE Phone END,
-  Zip =  CASE WHEN {zipcode} IS NOT NULL THEN {zipcode} ELSE Zip END,
-WHERE Email = {email};
+        SET 
+            Password = CASE WHEN {password} IS NOT NULL THEN {password} ELSE Password END,
+            FirstName = CASE WHEN {firstName} IS NOT NULL THEN {firstName} ELSE FirstName END,
+            LastName = CASE WHEN {lastName} IS NOT NULL THEN {lastName} ELSE LastName END,
+            City = CASE WHEN {city} IS NOT NULL THEN {city} ELSE City END,
+            State = CASE WHEN {state} IS NOT NULL THEN {state} ELSE State END,
+            Phone = CASE WHEN {phone} IS NOT NULL THEN {phone} ELSE Phone END,
+            Zip =  CASE WHEN {zipcode} IS NOT NULL THEN {zipcode} ELSE Zip END,
+        WHERE Email = {email};
         """
     )
+
 @moderators.route("/users/<email>", methods=["POST"])
 def new_test_user(email):
     """Creates a new test user"""
